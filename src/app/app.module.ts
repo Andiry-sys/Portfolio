2e29fa716be7bfd4f7f307d5f244de29223f67ae
Andriy-sys <marchycandriy@gmail.com>
2022-05-18 18:35:43 +0300

    Modern

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { SkilsComponent } from './skils/skils.component';
+import { PayForJobComponent } from './pay-for-job/pay-for-job.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PortfolioComponent,
-    SkilsComponent
+    SkilsComponent,
+    PayForJobComponent
    
   ],
   imports: [
